13dc9af59fbb7817a8438194fb7cbb190b668e06
TariqCO <tariq.official1712@gmail.com>
2026-02-09 13:04:06 +0500

    changes in code

--- programme2.py
+++ programme2.py
@@ -4,7 +4,7 @@ x = 32
 while True:
     r = int(input("Enter the range number, to find the secret number within the range: "))
     if r >= x:
-        for i in range(r):
+        for i in range(r + 1):
             if i == x:
                 print(f"Secret number found in this range --> secret num: {x}")
                 continue
@@ -14,7 +14,7 @@ while True:
     else:
         ans = input(f"\nSecret number not found in this range. Do you want to try again? (yes/no): ")
         if ans == "yes":
-            break
+            continue
         else:
             print("\nThank You.")
             break
