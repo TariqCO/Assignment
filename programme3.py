5ec0b1cf4e0312368581a29f2462a89ff73eb9a7
TariqCO <tariq.official1712@gmail.com>
2026-02-10 12:04:08 +0500

    add changes

--- programme3.py
+++ programme3.py
@@ -1,43 +1,57 @@
 
-stTariq=567
+stTariq=352
 stRafay=623
 stJohn=400
 
+# Dummy user details
+
+# name : Tariq Rasheed
+# class : 8
+# sec : B
+
+# name : Rafay Shaikh
+# class : 10
+# sec : C
+
+# name : Akber Ali
+# class : 10
+# sec : A
+
 print("\n=============== Find your Roll number ===============\n")
-
-name = input("Enter your name: ").lower()
-clss = input("Enter your class: ")
-sec =  input("Enter your Section: ")
-
 
 
 while True:
     print("\nSearch --> \n")
     
-    if (name == "tariq rasheed") and (clss == "8") and (sec == "b"):
-        for i in range(300 , 800 + 1):
+    fname = input("Enter your First name --> | ").capitalize()
+    lname = input("Enter your Last name --> | ").capitalize()
+    clss = input("Enter your Class --> | ")
+    sec =  input("Enter your Section --> | ").lower()
+    
+    if (fname == "Tariq") and (lname == "Rasheed") and (clss == "8") and (sec == "b"):
+        for i in range(200 , 800 + 1):
             if stTariq == i:
-                print(f"Name: {name}, Roll No.: {i}")
+                print(f"\nName: {fname} {lname}, Roll No: {i}")
                 break
             else:
                 print(f"searching list.... {i}")
                 continue
             
         break
-    elif (name == "rafay shaikh") and (clss == "10") and (sec == "c"):
+    elif (fname == "Rafay") and (lname == "Shaikh") and (clss == "10") and (sec == "c"):
         for i in range(300 , 800 + 1):
             if stRafay == i:
-                print(f"Name: {name}, Roll No.: {i}")
+                print(f"Name: {fname} {lname}, Roll No: {i}")
                 break
             else:
                 print(f"searching list.... {i}")
                 continue
             
         break
-    elif (name == "john doe") and (clss == "5") and (sec == "a"):
+    elif (fname == "Akber") and (lname == "Ali") and (clss == "10") and (sec == "a"):
         for i in range(300 , 800 + 1):
             if stJohn == i:
-                print(f"Name: {name}, Roll No.: {i}")
+                print(f"Name: {fname} {lname}, Roll No: {i}")
                 break
             else:
                 print(f"searching list.... {i}")
@@ -45,6 +59,11 @@ while True:
             
         break
     else:
-        print(f"There is no student with these details.")
-        break
+        print(f"\nThere is no student with these details.\nName: {fname} {lname}\nClass: {clss}\nSection: {sec}")
+        again= int(input("\nWant to  Search again? (1.Yes 2.No) --> | "))
+        if again == 1:
+            continue
+        else:
+            print("\nExiting.. Thanks!")
+            break
     
--- programme4.py
+++ programme4.py
@@ -3,7 +3,9 @@ std= int(input(f"Enter how many student are enrolled in this {batch}?: "))
 
 enrollUni= input("Enter unique code: ")
 
+
 for i in range(1, std + 1):
-    for j in range (1, 5 + 1, 2):
-        for k in range(4):
-            print(f"Unique Enrollemnt Id's: \n {i}-{batch}-{enrollUni}#{j}{k}")
+    if i > std/2:
+        print(f"Unique Enrollemnt Id's: \n{i}-{batch}-{enrollUni}#{i + 2}{i - 1}")
+    else:
+        print(f"Unique Enrollemnt Id's: \n{i}-{batch}-{enrollUni}@{i + 2}{i - 1}")
